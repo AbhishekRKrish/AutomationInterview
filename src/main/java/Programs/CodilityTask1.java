b04c32e5a90a219a921ae58e8dfef26dac070f2c
Abhishek_Rkrishna <abhishekrkrish@gmail.com>
2022-04-24 21:27:19 +0530

    Update CodilityTask1.java

--- src/main/java/Programs/CodilityTask1.java
+++ src/main/java/Programs/CodilityTask1.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 // Returns smallest missing integer value in an array
 
+//For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
 class Solution {
     public int solution(int[] A) {
         int smallestInt = 1;
